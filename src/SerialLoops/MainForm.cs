0462de12cd925335e2f76779897b568a39348e67
jonko0493 <jonko0493@protonmail.com>
2023-01-29 14:22:46 -0800

    Initial commit

---
+++ src/SerialLoops/MainForm.cs
@@ -0,0 +1,14 @@
+using Eto.Drawing;
+using Eto.Forms;
+using System;
+
+namespace SerialLoops
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
