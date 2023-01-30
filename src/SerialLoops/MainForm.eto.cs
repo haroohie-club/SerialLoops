221755ed68779efcecd374b43446e66e50c748f1
jonko0493 <jonko0493@protonmail.com>
2023-01-30 07:32:34 -0800

    Add open project dialog

--- src/SerialLoops/MainForm.eto.cs
+++ src/SerialLoops/MainForm.eto.cs
@@ -1,17 +1,20 @@
 using Eto.Drawing;
 using Eto.Forms;
 using System;
+using System.IO;
 
 namespace SerialLoops
 {
     public partial class MainForm : Form
     {
+        private const string BASE_TITLE = "Serial Loops";
+
         public Config CurrentConfig { get; set; }
         public Project OpenProject { get; set; }
 
         void InitializeComponent()
         {
-            Title = "Serial Loops";
+            Title = BASE_TITLE;
             ClientSize = new(769, 420);
             MinimumSize = new(769, 420);
             Padding = 10;
@@ -29,6 +32,9 @@ namespace SerialLoops
             Command newProject = new() { MenuText = "New Project", ToolBarText = "New Project" };
             newProject.Executed += NewProjectCommand_Executed;
 
+            Command openProject = new() { MenuText = "Open Project", ToolBarText = "Open Project" };
+            openProject.Executed += OpenProject_Executed;
+
             Command aboutCommand = new() { MenuText = "About..." };
             AboutDialog aboutDialog = new() { ProgramName = "Serial Loops", Developers = new string[] { "Jonko", "William" }, Copyright = "© Haroohie Translation Club, 2023", Website = new Uri("https://haroohie.club")  };
             aboutCommand.Executed += (sender, e) => aboutDialog.ShowDialog(this);
@@ -39,7 +45,7 @@ namespace SerialLoops
                 Items =
                 {
 					// File submenu
-					new SubMenuItem { Text = "&File", Items = { newProject } },
+					new SubMenuItem { Text = "&File", Items = { newProject, openProject } },
 					// new SubMenuItem { Text = "&Edit", Items = { /* commands/items */ } },
 					// new SubMenuItem { Text = "&View", Items = { /* commands/items */ } },
 				},
@@ -50,6 +56,11 @@ namespace SerialLoops
                 },
                 AboutItem = aboutCommand
             };
+        }
+
+        private void RenameWindow(string subtitle)
+        {
+            Title = $"{BASE_TITLE} - {subtitle}";
         }
 
         protected override void OnLoad(EventArgs e)
@@ -65,6 +76,17 @@ namespace SerialLoops
             if (projectCreationDialog.NewProject is not null)
             {
                 OpenProject = projectCreationDialog.NewProject;
+                RenameWindow(OpenProject.Name);
+            }
+        }
+
+        private void OpenProject_Executed(object sender, EventArgs e)
+        {
+            SelectFolderDialog selectFolderDialog = new() { Directory = CurrentConfig.ProjectsDirectory };
+            if (selectFolderDialog.ShowDialog(this) == DialogResult.Ok)
+            {
+                OpenProject = Project.OpenProject(Path.GetFileNameWithoutExtension(selectFolderDialog.Directory), CurrentConfig);
+                RenameWindow(OpenProject.Name);
             }
         }
     }
--- src/SerialLoops/Project.cs
+++ src/SerialLoops/Project.cs
@@ -9,14 +9,23 @@ namespace SerialLoops
         public string BaseDirectory => Path.Combine(MainDirectory, "base");
         public string IterativeDirectory => Path.Combine(MainDirectory, "iterative");
 
-        public Project(string name, Config config)
+        public Project(string name, Config config, bool createDirectories = true)
         {
             Name = name;
             MainDirectory = Path.Combine(config.ProjectsDirectory, name);
 
-            Directory.CreateDirectory(MainDirectory);
-            Directory.CreateDirectory(BaseDirectory);
-            Directory.CreateDirectory(IterativeDirectory);
+            if (createDirectories)
+            {
+                Directory.CreateDirectory(MainDirectory);
+                Directory.CreateDirectory(BaseDirectory);
+                Directory.CreateDirectory(IterativeDirectory);
+            }
+        }
+
+        public static Project OpenProject(string name, Config config)
+        {
+            Project project = new(name, config, createDirectories: false);
+            return project;
         }
     }
 }
