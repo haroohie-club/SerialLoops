77cdf592e436ce66ae3a06d944c9256870b5819e
Jonko <69772986+jonko0493@users.noreply.github.com>
2023-02-02 15:53:07 -0800

    Add character sprite items (#16)

--- src/SerialLoops.Lib/Items/CharacterSpriteItem.cs
+++ src/SerialLoops.Lib/Items/CharacterSpriteItem.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using HaruhiChokuretsuLib.Archive.Data;
+using SkiaSharp;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SerialLoops.Lib.Items
 {
-    internal class CharacterSpriteItem
+    public class CharacterSpriteItem : Item
     {
+        public CharacterSprite Sprite { get; set; }
+
+        public CharacterSpriteItem(CharacterSprite sprite) : base($"SPR_{sprite.Character}_{sprite.MouthAnimationIndex}_{sprite.EyeAnimationIndex}", ItemType.Character_Sprite)
+        {
+            Sprite = sprite;
+        }
+
+        public override void Refresh(Project project)
+        {
+        }
+
+        public List<(SKBitmap frame, int timing)> GetClosedMouthAnimation(Project project)
+        {
+            MessageInfoFile messageInfo = project.Dat.Files.First(f => f.Name == "MESSINFOS").CastTo<MessageInfoFile>();
+            return Sprite.GetClosedMouthAnimation(project.Grp, messageInfo);
+        }
+
+        public List<(SKBitmap frame, int timing)> GetLipFlapAnimation(Project project)
+        {
+            MessageInfoFile messageInfo = project.Dat.Files.First(f => f.Name == "MESSINFOS").CastTo<MessageInfoFile>();
+            return Sprite.GetLipFlapAnimation(project.Grp, messageInfo);
+        }
     }
 }
