9e8cbcf3d2ff9af2e6b1320e8b740c93ed9888af
Jonko <69772986+jonko0493@users.noreply.github.com>
2023-02-02 17:10:49 -0800

    Add script references to chibis, sprites, and backgrounds (#17)

--- src/SerialLoops.Lib/Items/ChibiItem.cs
+++ src/SerialLoops.Lib/Items/ChibiItem.cs
@@ -1,26 +1,34 @@
 ﻿using HaruhiChokuretsuLib.Archive;
 using HaruhiChokuretsuLib.Archive.Data;
+using HaruhiChokuretsuLib.Archive.Event;
 using HaruhiChokuretsuLib.Archive.Graphics;
 using SkiaSharp;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace SerialLoops.Lib.Items
 {
     public class ChibiItem : Item
     {
         public Chibi Chibi { get; set; }
+        public int ChibiIndex { get; set; }
         public List<(string Name, ChibiEntry Chibi)> ChibiEntries { get; set; } = new();
         public List<IEnumerable<(SKBitmap Frame, int Timing)>> ChibiAnimations { get; set; } = new();
+        public (string ScriptName, ScriptCommandInvocation command)[] ScriptUses { get; set; }
 
         public ChibiItem(Chibi chibi, Project project) : base($"CHIBI{chibi.ChibiEntries[0].Animation}", ItemType.Chibi)
         {
+            List<string> chibiIndices = new() { "", "KYN", "HAL", "MIK", "NAG", "KOI" };
+
             Chibi = chibi;
             string firstAnimationName = project.Grp.Files.First(f => f.Index == Chibi.ChibiEntries[0].Animation).Name;
             Name = $"CHIBI_{firstAnimationName[0..firstAnimationName.IndexOf('_')]}";
+            ChibiIndex = chibiIndices.IndexOf(firstAnimationName[0..3]);
             ChibiEntries.AddRange(Chibi.ChibiEntries.Where(c => c.Animation > 0)
                 .Select(c => (project.Grp.Files.First(f => f.Index == c.Animation).Name[0..^3], c)));
             ChibiAnimations.AddRange(ChibiEntries.Select(c => GetChibiAnimation(c.Name, project.Grp)));
+            PopulateScriptUses(project.Evt);
         }
 
         private IEnumerable<(SKBitmap Frame, int Timing)> GetChibiAnimation(string entryName, ArchiveFile<GraphicsFile> grp)
@@ -37,6 +45,19 @@ namespace SerialLoops.Lib.Items
         public override void Refresh(Project project)
         {
             GetChibiAnimation(Name, project.Grp);
+            PopulateScriptUses(project.Evt);
+        }
+
+        public void PopulateScriptUses(ArchiveFile<EventFile> evt)
+        {
+            string[] chibiCommands = new string[] { "CHIBI_ENTEREXIT" };
+
+            var list = evt.Files.SelectMany(e =>
+                e.ScriptSections.SelectMany(sec =>
+                    sec.Objects.Where(c => chibiCommands.Contains(c.Command.Mnemonic)).Select(c => (e.Name[0..^1], c))))
+                .Where(t => t.c.Parameters[0] == ChibiIndex).ToList();
+
+            ScriptUses = list.ToArray();
         }
     }
 }
