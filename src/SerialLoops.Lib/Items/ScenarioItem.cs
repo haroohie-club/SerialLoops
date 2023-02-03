2e00f57499d946dec82082c749f56a72fdfd13e2
jonko0493 <jonko0493@protonmail.com>
2023-02-02 16:59:38 -0800

    Add scaffolding for scenario editor

---
+++ src/SerialLoops.Lib/Items/ScenarioItem.cs
@@ -0,0 +1,23 @@
+﻿using HaruhiChokuretsuLib.Archive.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialLoops.Lib.Items
+{
+    public class ScenarioItem : Item
+    {
+        public ScenarioStruct Scenario { get; set; }
+
+        public ScenarioItem(ScenarioStruct scenario) : base("Scenario", ItemType.Scenario)
+        {
+            Scenario = scenario;
+        }
+
+        public override void Refresh(Project project)
+        {
+        }
+    }
+}
---
+++ src/SerialLoops/Editors/ScenarioEditor.cs
@@ -0,0 +1,32 @@
+﻿using Eto.Forms;
+using HaruhiChokuretsuLib.Util;
+using SerialLoops.Lib.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialLoops.Editors
+{
+    public class ScenarioEditor : Editor
+    {
+        private ScenarioItem _scenario;
+
+        public ScenarioEditor(ScenarioItem item, ILogger log) : base(item, log)
+        {
+        }
+
+        public override Panel GetEditorPanel()
+        {
+            _scenario = (ScenarioItem)Description;
+            StackLayout mainLayout = new()
+            {
+                Orientation = Orientation.Horizontal,
+                Spacing = 20,
+            };
+
+            return mainLayout;
+        }
+    }
+}
