bb960eac62dfaa051a2d761481fa2c4203b50fcd
William278 <will27528@gmail.com>
2023-02-10 16:22:47 +0000

    Add recent projects, build & run, toolbar icons

---
+++ src/SerialLoops.Lib/RecentProjects.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using HaruhiChokuretsuLib.Util;
+
+namespace SerialLoops.Lib
+{
+    public class RecentProjects
+    {
+        private const int MAX_RECENT_PROJECTS = 10; 
+        public string RecentProjectsPath { get; set; }
+        public List<string> Projects { get; set; }
+
+        public void Save(ILogger log)
+        {
+            log.Log($"Saving recent projects to '{RecentProjectsPath}'...");
+            IO.WriteStringFile(RecentProjectsPath, JsonSerializer.Serialize(this), log);
+        }
+        
+        public static RecentProjects LoadRecentProjects(ILogger log)
+        {
+            string recentProjectsJson = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "recent_projects.json");
+            if (!File.Exists(recentProjectsJson))
+            {
+                log.Log($"Creating default recent projects cache at '{recentProjectsJson}'...");
+                RecentProjects defaultRecentProjects = GetDefault();
+                defaultRecentProjects.RecentProjectsPath = recentProjectsJson;
+                IO.WriteStringFile(recentProjectsJson, JsonSerializer.Serialize(defaultRecentProjects), log);
+                return defaultRecentProjects;
+            }
+
+            try
+            {
+                RecentProjects recentProjects = JsonSerializer.Deserialize<RecentProjects>(File.ReadAllText(recentProjectsJson));
+                recentProjects.RecentProjectsPath = recentProjectsJson;
+                return recentProjects;
+            }
+            catch (JsonException exc)
+            {
+                log.LogError($"Exception occurred while parsing recent_projects.json!\n{exc.Message}\n\n{exc.StackTrace}");
+                RecentProjects defaultRecentProjects = GetDefault();
+                IO.WriteStringFile(recentProjectsJson, JsonSerializer.Serialize(defaultRecentProjects), log);
+                return defaultRecentProjects;
+            }
+        }
+        
+        private static RecentProjects GetDefault()
+        {
+            return new()
+            {
+                Projects = new List<string>()
+            };
+        }
+
+        public void AddProject(string projectPath)
+        {
+            if (Projects.Contains(projectPath))
+            {
+                Projects.Remove(projectPath);
+            }
+            if (Projects.Count >= MAX_RECENT_PROJECTS)
+            {
+                Projects.RemoveAt(Projects.Count - 1);
+            }
+            Projects.Insert(0, projectPath);
+        }
+        
+    }   
+}
