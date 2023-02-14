25063c1134c74024c4a238ea0aee20ae6ad97551
jonko0493 <jonko0493@protonmail.com>
2023-02-13 19:05:19 -0800

    Increase buffer size (which maybe fixes it?)

--- src/SerialLoops.Gtk/SoundPlayerHandler.cs
+++ src/SerialLoops.Gtk/SoundPlayerHandler.cs
@@ -20,7 +20,7 @@ namespace SerialLoops.Gtk
         public void Initialize(IWaveProvider waveProvider)
         {
             WaveProvider = waveProvider;
-            _player = new(new(), 4096);
+            _player = new(new(), 8192);
             _player.Init(WaveProvider);
         }
 
