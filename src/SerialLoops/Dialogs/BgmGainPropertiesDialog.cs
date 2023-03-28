fc5de7449f5a04f35353029774f91034e3a19ae8
jonko0493 <jonko0493@protonmail.com>
2023-03-27 17:35:09 -0700

    Add gain properties skeleton

---
+++ src/SerialLoops/Dialogs/BgmGainPropertiesDialog.cs
@@ -0,0 +1,22 @@
+﻿using Eto.Forms;
+using HaruhiChokuretsuLib.Util;
+using NAudio.Wave;
+using SerialLoops.Lib.Items;
+
+namespace SerialLoops.Dialogs
+{
+    public class BgmGainPropertiesDialog : Dialog
+    {
+
+    }
+
+    public class BgmVolumePreviewItem : ISoundItem
+    {
+        public ISampleProvider Provider { get; set; }
+
+        public IWaveProvider GetWaveProvider(ILogger log, bool loop)
+        {
+            return Provider.ToWaveProvider16();
+        }
+    }
+}
