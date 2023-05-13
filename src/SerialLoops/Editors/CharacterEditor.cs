3477f3fd97df9c2b4a61b2a72e4e3f5567bfb0db
jonko0493 <jonko0493@protonmail.com>
2023-05-13 05:31:10 -0700

    Working MS Gothic font for nameplates

--- src/SerialLoops/Editors/CharacterEditor.cs
+++ src/SerialLoops/Editors/CharacterEditor.cs
@@ -4,10 +4,10 @@ using SerialLoops.Lib;
 using SerialLoops.Lib.Items;
 using SerialLoops.Utility;
 using SkiaSharp;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Topten.RichTextKit;
 
 namespace SerialLoops.Editors
@@ -41,18 +41,12 @@ namespace SerialLoops.Editors
                 },
             };
 
-            using Stream fontStream = Assembly.GetCallingAssembly().GetManifestResourceStream("SerialLoops.Graphics.msgothic-variablewidth.ttf");
+            using Stream fontStream = Assembly.GetCallingAssembly().GetManifestResourceStream("SerialLoops.Graphics.MS-Gothic-Haruhi.ttf");
             SKTypeface font = SKTypeface.FromStream(fontStream);
             if (!CustomFontMapper.HasFont())
             {
                 CustomFontMapper.AddFont(font);
             }
-            SKFont trueFont = new(font, 13.0f);
-            float[] widths = new float[26];
-            SKRect[] bounds = new SKRect[26];
-            trueFont.GetGlyphWidths(font.GetGlyphs("abcdefghijklmnopqrstuvwxyz"), widths, bounds);
-            _log.Log(string.Join(", ", widths));
-            _log.Log(string.Join(", ", bounds));
 
             using Stream blankNameplateStream = Assembly.GetCallingAssembly().GetManifestResourceStream("SerialLoops.Graphics.BlankNameplate.png");
             SKBitmap blankNameplate = SKBitmap.Decode(blankNameplateStream);
@@ -61,35 +55,7 @@ namespace SerialLoops.Editors
 
             characterBox.TextChanged += (sender, args) =>
             {
-                SKBitmap newNameplate = new(64, 16);
-                SKCanvas newCanvas = new(newNameplate);
-                newCanvas.DrawBitmap(blankNameplate, new SKPoint(0, 0));
-                newCanvas.DrawBitmap(blankNameplateBaseArrow, new SKPoint(0, 3),
-                    new SKPaint()
-                    {
-                        ColorFilter = SKColorFilter.CreateColorMatrix(new float[]
-                        {
-                            plateColorPicker.Value.ToSKColor().Red / 255.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-                            0.0f, plateColorPicker.Value.ToSKColor().Green / 255.0f, 0.0f, 0.0f, 0.0f,
-                            0.0f, 0.0f, plateColorPicker.Value.ToSKColor().Blue / 255.0f, 0.0f, 0.0f,
-                            0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-                        })
-                    });
-                newCanvas.DrawLine(new(0, 15), new(59, 15), new() { Color = plateColorPicker.Value.ToSKColor() });
-                TextBlock textBlock = new() { Alignment = Topten.RichTextKit.TextAlignment.Center, FontMapper = new CustomFontMapper(), MaxWidth = 52, MaxHeight = 13 };
-                textBlock.AddText(characterBox.Text,
-                    new Style()
-                    {
-                        TextColor = textColorPicker.Value.ToSKColor(),
-                        FontFamily = font.FamilyName,
-                        FontSize = 13.0f,
-                        HaloWidth = 0.5f,
-                        HaloColor = new SKColor(128, 128, 128),
-                    });
-                textBlock.Paint(newCanvas, new SKPoint(7, 2), new() { Edging = SKFontEdging.SubpixelAntialias, SubpixelPositioning = true });
-                newCanvas.Flush();
-                nameplatePreviewLayout.Items.Clear();
-                nameplatePreviewLayout.Items.Add(new SKGuiImage(newNameplate));
+                UpdatePreview(nameplatePreviewLayout, characterBox.Text, font, blankNameplate, blankNameplateBaseArrow, textColorPicker.Value.ToSKColor(), plateColorPicker.Value.ToSKColor());
             };
 
             return new StackLayout
@@ -103,6 +69,39 @@ namespace SerialLoops.Editors
                     ControlGenerator.GetControlWithLabel("Text Timer", new NumericStepper { Value = _character.MessageInfo.TextTimer }),
                 },
             };
+        }
+
+        private void UpdatePreview(StackLayout nameplatePreviewLayout, string name, SKTypeface font, SKBitmap blankNameplate, SKBitmap blankNameplateBaseArrow, SKColor textColor, SKColor plateColor)
+        {
+            SKBitmap newNameplate = new(64, 16);
+            SKCanvas newCanvas = new(newNameplate);
+            newCanvas.DrawBitmap(blankNameplate, new SKPoint(0, 0));
+            newCanvas.DrawBitmap(blankNameplateBaseArrow, new SKPoint(0, 3),
+                new SKPaint()
+                {
+                    ColorFilter = SKColorFilter.CreateColorMatrix(new float[]
+                    {
+                        plateColor.Red / 255.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                        0.0f, plateColor.Green / 255.0f, 0.0f, 0.0f, 0.0f,
+                        0.0f, 0.0f, plateColor.Blue / 255.0f, 0.0f, 0.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+                    })
+                });
+            newCanvas.DrawLine(new(0, 15), new(59, 15), new() { Color = plateColor });
+            TextBlock textBlock = new() { Alignment = Topten.RichTextKit.TextAlignment.Center, FontMapper = new CustomFontMapper(), MaxWidth = 52, MaxHeight = 13 };
+            textBlock.AddText(name,
+                new Style()
+                {
+                    TextColor = textColor,
+                    FontFamily = font.FamilyName,
+                    FontSize = 13.0f,
+                    HaloWidth = 0.5f,
+                    HaloColor = new SKColor(128, 128, 128),
+                });
+            textBlock.Paint(newCanvas, new SKPoint(7, 2), new() { Edging = SKFontEdging.SubpixelAntialias, SubpixelPositioning = true });
+            newCanvas.Flush();
+            nameplatePreviewLayout.Items.Clear();
+            nameplatePreviewLayout.Items.Add(new SKGuiImage(newNameplate));
         }
 
         private class CustomFontMapper : FontMapper
