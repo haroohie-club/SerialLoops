70452011c33e4ffc5c9dbf3e5d3973d77f2e7f92
jonko0493 <jonko0493@protonmail.com>
2023-06-20 09:57:11 -0700

    Final 8) fixup on the sprite display bug

--- src/SerialLoops/Editors/ScriptEditor.cs
+++ src/SerialLoops/Editors/ScriptEditor.cs
@@ -1791,7 +1791,7 @@ namespace SerialLoops.Editors
                                 case SpriteExitScriptParameter.SpriteExitTransition.SLIDE_FROM_CENTER_TO_RIGHT_FADE_OUT:
                                 case SpriteExitScriptParameter.SpriteExitTransition.FADE_OUT_CENTER:
                                 case SpriteExitScriptParameter.SpriteExitTransition.FADE_OUT_LEFT:
-                                    if (sprites.ContainsKey(prevCharacter) && previousSprites.ContainsKey(prevCharacter) && previousSprites[prevCharacter].Sprite.Sprite.Character == prevCharacter.MessageInfo.Character)
+                                    if (sprites.ContainsKey(prevCharacter) && previousSprites.ContainsKey(prevCharacter) && ((SpriteScriptParameter)previousCommand.Parameters[1]).Sprite?.Sprite?.Character == prevCharacter.MessageInfo.Character)
                                     {
                                         sprites.Remove(prevCharacter);
                                         previousSprites.Remove(prevCharacter);
