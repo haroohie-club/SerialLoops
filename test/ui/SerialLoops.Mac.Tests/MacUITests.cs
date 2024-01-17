b131080f0e24a6c620d06f180f5d6b2c01d7f12c
jonko0493 <jonko0493@protonmail.com>
2024-01-16 23:16:38 -0800

    Try it a different way ig

--- test/ui/SerialLoops.Mac.Tests/MacUITests.cs
+++ test/ui/SerialLoops.Mac.Tests/MacUITests.cs
@@ -220,11 +220,12 @@ namespace SerialLoops.Mac.Tests
             AppiumElement image = _driver.FindElement(MobileBy.IosClassChain("**/XCUIElementTypeImage"));
             Actions actions = new(_driver);
             actions.MoveToElement(image); // this is the center of the image
+            actions.Build().Perform();
+            actions = new(_driver);
             actions.MoveByOffset(-image.Size.Width / 2 - 30, -image.Size.Height / 2 - 30); // we move to the top left corner, subtracted so we're slightly closer to the center
             actions.ClickAndHold();
             actions.MoveByOffset(300, 60);
             actions.Release();
-            actions.Build().Perform();
             Thread.Sleep(200);
             _driver.GetAndSaveScreenshot(Path.Combine(testArtifactsFolder, $"{bgName}_moved.png"));
 
