f6ac9a9fbf8b8533a6df11b461a5f40ca826a933
jonko0493 <jonko0493@protonmail.com>
2024-01-20 14:44:15 -0800

    Add save editor dialog

---
+++ src/SerialLoops/Dialogs/SaveEditorDialog.cs
@@ -0,0 +1,13 @@
+﻿using Eto.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialLoops.Dialogs
+{
+    public class SaveEditorDialog : Dialog
+    {
+    }
+}
