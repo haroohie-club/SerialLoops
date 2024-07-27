efbd974b4f278372e012f49b930af2a715c4c1b8
jonko0493 <jonko0493@protonmail.com>
2024-07-27 14:02:51 -0700

    Attempt to fix bug on mac

--- src/SerialLoops/Views/MainWindow.axaml.cs
+++ src/SerialLoops/Views/MainWindow.axaml.cs
@@ -22,50 +22,50 @@ namespace SerialLoops.Views
             base.Show();
             _viewModel = (MainWindowViewModel)DataContext;
             _viewModel.Initialize(this);
-            NativeMenu.SetMenu(this, GetInitialMenu());
+            NativeMenu.SetMenu(this, GetInitialMenu(NativeMenu.GetMenu(this)));
         }
 
-        private NativeMenu GetInitialMenu()
+        private NativeMenu GetInitialMenu(NativeMenu menu)
         {
-            return [
-                new NativeMenuItem()
-                {
-                    Header = Strings._File,
-                    Menu =
-                    [
-                        new NativeMenuItem()
-                        {
-                            Header = Strings.New_Project___,
-                            Icon = ControlGenerator.GetIcon("New", _viewModel.Log),
-                        },
-                        new NativeMenuItem()
-                        {
-                            Header = Strings.Open_Project,
-                            Icon = ControlGenerator.GetIcon("Open", _viewModel.Log),
-                        },
-                        _viewModel.RecentProjectsMenu,
-                        new NativeMenuItemSeparator(),
-                        new NativeMenuItem()
-                        {
-                            Header = Strings.Edit_Save_File,
-                            Icon = ControlGenerator.GetIcon("Edit_Save", _viewModel.Log),
-                        }
-                    ]
-                },
-                new NativeMenuItem()
-                {
-                    Header = Strings._Help,
-                    Menu =
-                    [
-                        new NativeMenuItem()
-                        {
-                            Header = Strings.About___,
-                            Icon = ControlGenerator.GetIcon("Help", _viewModel.Log),
-                            Command = _viewModel.AboutCommand,
-                        },
-                    ]
-                }
-                ];
+            menu.Items.Clear();
+            menu.Items.Add(new NativeMenuItem()
+            {
+                Header = Strings._File,
+                Menu =
+                [
+                    new NativeMenuItem()
+                    {
+                        Header = Strings.New_Project___,
+                        Icon = ControlGenerator.GetIcon("New", _viewModel.Log),
+                    },
+                    new NativeMenuItem()
+                    {
+                        Header = Strings.Open_Project,
+                        Icon = ControlGenerator.GetIcon("Open", _viewModel.Log),
+                    },
+                    _viewModel.RecentProjectsMenu,
+                    new NativeMenuItemSeparator(),
+                    new NativeMenuItem()
+                    {
+                        Header = Strings.Edit_Save_File,
+                        Icon = ControlGenerator.GetIcon("Edit_Save", _viewModel.Log),
+                    }
+                ]
+            });
+            menu.Items.Add(new NativeMenuItem()
+            {
+                Header = Strings._Help,
+                Menu =
+                [
+                    new NativeMenuItem()
+                    {
+                        Header = Strings.About___,
+                        Icon = ControlGenerator.GetIcon("Help", _viewModel.Log),
+                        Command = _viewModel.AboutCommand,
+                    },
+                ]
+            });
+            return menu;
         }
 
         private async void Window_Closing(object? sender, WindowClosingEventArgs e)
