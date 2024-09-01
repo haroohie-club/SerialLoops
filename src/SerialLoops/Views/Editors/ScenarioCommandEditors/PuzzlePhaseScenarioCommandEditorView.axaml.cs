df6322e160b886317d3d1a1b2f01051d25037e99
jonko0493 <jonko0493@protonmail.com>
2024-09-01 00:11:12 -0700

    Fixup some formatting

--- src/SerialLoops/Views/Editors/ScenarioCommandEditors/PuzzlePhaseScenarioCommandEditorView.axaml.cs
+++ src/SerialLoops/Views/Editors/ScenarioCommandEditors/PuzzlePhaseScenarioCommandEditorView.axaml.cs
@@ -9,5 +9,4 @@ namespace SerialLoops.Views.Editors.ScenarioCommandEditors
             InitializeComponent();
         }
     }
-
 }
--- src/SerialLoops/Views/Editors/ScenarioCommandEditors/ScenarioCommandEditorView.axaml.cs
+++ src/SerialLoops/Views/Editors/ScenarioCommandEditors/ScenarioCommandEditorView.axaml.cs
@@ -11,5 +11,4 @@ namespace SerialLoops.Views.Editors.ScenarioCommandEditors
             InitializeComponent();
         }
     }
-
 }
