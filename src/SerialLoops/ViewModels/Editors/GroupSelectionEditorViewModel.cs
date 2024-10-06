2615d5b3d2a8027ec2c41b4466badf909a4b4a02
Jonko <69772986+jonko0493@users.noreply.github.com>
2024-10-06 12:54:55 -0700

    Add group selection editor to Avalonia branch (#396)

---
+++ src/SerialLoops/ViewModels/Editors/GroupSelectionEditorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using HaruhiChokuretsuLib.Util;
+using ReactiveUI.Fody.Helpers;
+using SerialLoops.Lib;
+using SerialLoops.Lib.Items;
+
+namespace SerialLoops.ViewModels.Editors
+{
+    public class GroupSelectionEditorViewModel : EditorViewModel
+    {
+        [Reactive]
+        public GroupSelectionItem GroupSelection { get; set; }
+        public Project OpenProject { get; set; }
+
+        public GroupSelectionEditorViewModel(GroupSelectionItem groupSelection, MainWindowViewModel window, ILogger log) : base(groupSelection, window, log)
+        {
+            GroupSelection = groupSelection;
+            OpenProject = window.OpenProject;
+        }
+    }
+}
