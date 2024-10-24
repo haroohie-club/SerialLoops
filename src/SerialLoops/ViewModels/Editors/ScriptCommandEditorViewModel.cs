1dd60814994b5bf8eeec6c1e7c1f52a0ce983fc1
jonko0493 <jonko0493@protonmail.com>
2024-10-24 07:54:13 -0700

    Initial script editor work

---
+++ src/SerialLoops/ViewModels/Editors/ScriptCommandEditorViewModel.cs
@@ -0,0 +1,30 @@
+﻿using HaruhiChokuretsuLib.Archive.Event;
+using System.Collections.Generic;
+using HaruhiChokuretsuLib.Util;
+using SerialLoops.Lib.Items;
+using SerialLoops.Lib.Script;
+
+namespace SerialLoops.ViewModels.Editors
+{
+    public class ScriptCommandEditorViewModel : EditorViewModel
+    {
+        private ScriptItem _script;
+        private Dictionary<ScriptSection, List<ScriptItemCommand>> _commands = [];
+
+        public ScriptCommandEditorViewModel(ScriptItem script, MainWindowViewModel window, ILogger log) : base(script, window, log)
+        {
+            _script = script;
+            PopulateScriptCommands();
+            _script.CalculateGraphEdges(_commands, _log);
+        }
+
+        public void PopulateScriptCommands(bool refresh = false)
+        {
+            if (refresh)
+            {
+                _script.Refresh(_project, _log);
+            }
+            _commands = _script.GetScriptCommandTree(_project, _log);
+        }
+    }
+}
