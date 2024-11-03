81d20390f3980e8bbfd02373427a10d5005f49ba
jonko0493 <jonko0493@protonmail.com>
2024-11-03 11:02:30 -0800

    Add SCREEN_FADEOUT command

--- src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
+++ src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
@@ -135,6 +135,7 @@ namespace SerialLoops.ViewModels.Editors
                     CommandVerb.PIN_MNL => new PinMnlScriptCommandEditorViewModel(_selectedCommand, this, _window.OpenProject),
                     CommandVerb.BG_DISP => new BgDispScriptCommandEditorViewModel(_selectedCommand, this, _window),
                     CommandVerb.SCREEN_FADEIN => new ScreenFadeInScriptCommandEditorViewModel(_selectedCommand, this),
+                    CommandVerb.SCREEN_FADEOUT => new ScreenFadeOutScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.REMOVED => new EmptyScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.SND_STOP => new EmptyScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.SCREEN_SHAKE_STOP => new EmptyScriptCommandEditorViewModel(_selectedCommand, this),
