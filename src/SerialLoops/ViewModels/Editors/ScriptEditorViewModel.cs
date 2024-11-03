7d95d46841be561ea6dc6bac4ea2c4ed95a0c02a
jonko0493 <jonko0493@protonmail.com>
2024-11-03 11:16:26 -0800

    Add SCREEN_FLASH command

--- src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
+++ src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
@@ -136,6 +136,7 @@ namespace SerialLoops.ViewModels.Editors
                     CommandVerb.BG_DISP => new BgDispScriptCommandEditorViewModel(_selectedCommand, this, _window),
                     CommandVerb.SCREEN_FADEIN => new ScreenFadeInScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.SCREEN_FADEOUT => new ScreenFadeOutScriptCommandEditorViewModel(_selectedCommand, this),
+                    CommandVerb.SCREEN_FLASH => new ScreenFlashScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.REMOVED => new EmptyScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.SND_STOP => new EmptyScriptCommandEditorViewModel(_selectedCommand, this),
                     CommandVerb.SCREEN_SHAKE_STOP => new EmptyScriptCommandEditorViewModel(_selectedCommand, this),
