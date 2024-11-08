bf6e4aac7052fc7379596a032b398939abd6a834
jonko0493 <jonko0493@protonmail.com>
2024-11-08 12:29:46 -0800

    Add script for macOS pkg builder

---
+++ install/macos/serial_loops_pkg_builder.py
@@ -0,0 +1,23 @@
+from macos_pkg_builder import Packages
+from sys import argv
+
+ver = argv[1]
+
+with open("welcome.md") as welcome_file:
+    welcome_md = welcome_file.read()
+with open("../../README.md") as readme_file:
+    readme_md = readme_file.read()
+with open("../../LICENSE") as license_file:
+    license_md = license_file.read()
+
+pkg_obj = Packages(
+    pkg_output="SerialLoops.pkg",
+    pkg_bundle_id="club.haroohie.serialloopsinstaller",
+    pkg_version=ver,
+    pkg_welcome=welcome_md,
+    pkg_readme=readme_md,
+    pkg_license=license_md,
+    pkg_title="Serial Loops"
+)
+
+assert pkg_obj.build() is True
---
+++ install/macos/welcome.md
@@ -0,0 +1,9 @@
+# Welcome to Serial Loops!
+This application will guide you through installing Serial Loops and all of its dependencies.
+
+The following applications will be installed:
+* devkitARM by devkitPro
+* make
+* Serial Loops
+
+If any of these are already installed, they will be skipped!
