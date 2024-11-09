c0395eb4f0325d02b338dba93a5bd3633e23e0ff
jonko0493 <jonko0493@protonmail.com>
2024-11-08 22:20:59 -0800

    Dump log when installing devkitpro pacman on macOS

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d "/opt/devkitpro/devkitARM" ]; then
-    installer -pkg devkitpro-pacman-installer.pkg -target /
+    installer -pkg devkitpro-pacman-installer.pkg -target / -dumplog
     xcode-select --install
     dkp-pacman -Sy
     dkp-pacman -S --noconfirm nds-dev
