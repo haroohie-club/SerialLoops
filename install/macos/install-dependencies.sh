4215d9cfbdb86d2902d3e61ad5600a7b346c9fdb
jonko0493 <jonko0493@protonmail.com>
2024-11-09 00:31:52 -0800

    Few more flatpak fixes, possible fix for pkg installer

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -2,7 +2,9 @@
 
 if [ ! -d "/opt/devkitpro/devkitARM" ]; then
     installer -pkg devkitpro-pacman-installer.pkg -target / -dumplog >> /var/tmp/SerialLoopsInstaller.log
-    xcode-select --install || echo "xcode tools already installed" >> /var/tmp/SerialLoopsInstaller.log
+    xcode-select --install || true >> /var/tmp/SerialLoopsInstaller.log
     dkp-pacman -Sy --noconfirm >> /var/tmp/SerialLoopsInstaller.log
     dkp-pacman -S --noconfirm nds-dev >> /var/tmp/SerialLoopsInstaller.log
 fi
+
+exit 0
