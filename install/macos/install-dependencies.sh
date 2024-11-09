dccca2643506cfa37a2308e09ac9c175e20578ab
jonko0493 <jonko0493@protonmail.com>
2024-11-08 23:32:15 -0800

    Some initial flatpak work, maybe fix the pkg installer

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -2,5 +2,7 @@
 
 if [ ! -d "/opt/devkitpro/devkitARM" ]; then
     installer -pkg devkitpro-pacman-installer.pkg -target /
-    xcode-select --install
+    xcode-select --install || echo "xcode tools already installed"
+    dkp-pacman -Sy --noconfirm
+    dkp-pacman -S --noconfirm nds-dev
 fi
--- install/macos/install-devkitarm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "/opt/devkitpro/devkitARM" ]; then
-    dkp-pacman -Sy --noconfirm
-    dkp-pacman -S --noconfirm nds-dev
-fi
