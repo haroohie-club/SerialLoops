014c2eadbb5b9cd079bcd4ac7d6561e9e43e6330
jonko0493 <jonko0493@protonmail.com>
2024-11-08 21:59:39 -0800

    Fixup pacman incantation

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -4,5 +4,5 @@ if [ ! -d "/opt/devkitpro/devkitARM" ]; then
     installer -pkg devkitpro-pacman-installer.pkg -target /
     xcode-select --install
     dkp-pacman -Sy
-    dkp-pacman -S --no-confirm nds-dev
+    dkp-pacman -S --noconfirm nds-dev
 fi
