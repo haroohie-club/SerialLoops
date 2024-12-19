1e0f6ca594f88da2f7382654928f9fdf7e05a70f
jonko0493 <jonko0493@protonmail.com>
2024-12-18 18:50:27 -0800

    Finish up layout editor

--- src/SerialLoops/Assets/Strings.Designer.cs
+++ src/SerialLoops/Assets/Strings.Designer.cs
@@ -2484,6 +2484,15 @@ namespace SerialLoops.Assets {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Export Layout Preview.
+        /// </summary>
+        public static string Export_Layout_Preview {
+            get {
+                return ResourceManager.GetString("Export Layout Preview", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Export Patch.
         /// </summary>
         public static string Export_Patch {
@@ -2516,6 +2525,15 @@ namespace SerialLoops.Assets {
         public static string Export_SFX {
             get {
                 return ResourceManager.GetString("Export SFX", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Export Source Preview.
+        /// </summary>
+        public static string Export_Source_Preview {
+            get {
+                return ResourceManager.GetString("Export Source Preview", resourceCulture);
             }
         }
         
--- src/SerialLoops/ViewModels/Editors/LayoutEditorViewModel.cs
+++ src/SerialLoops/ViewModels/Editors/LayoutEditorViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Avalonia.Platform.Storage;
 using HaruhiChokuretsuLib.Archive.Graphics;
 using HaruhiChokuretsuLib.Util;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using SerialLoops.Assets;
 using SerialLoops.Lib.Items;
+using SerialLoops.Utility;
 using SkiaSharp;
 
 namespace SerialLoops.ViewModels.Editors;
@@ -13,6 +19,7 @@ namespace SerialLoops.ViewModels.Editors;
 public class LayoutEditorViewModel : EditorViewModel
 {
     private LayoutItem _layout;
+    private MainWindowViewModel _mainWindow;
     public ObservableCollection<LayoutEntryWithImage> LayoutEntries { get; }
 
     private LayoutEntryWithImage _selectedLayoutEntry;
@@ -30,10 +37,48 @@ public class LayoutEditorViewModel : EditorViewModel
         }
     }
 
+    public ICommand ExportLayoutCommand { get; }
+    public ICommand ExportSourceCommand { get; }
+
     public LayoutEditorViewModel(LayoutItem item, MainWindowViewModel window, ILogger log) : base(item, window, log)
     {
         _layout = item;
+        _mainWindow = window;
         LayoutEntries = new(_layout.Layout.LayoutEntries.Skip(_layout.StartEntry).Take(_layout.NumEntries).Select((_, i) => new LayoutEntryWithImage(_layout, i + _layout.StartEntry)));
+        ExportLayoutCommand = ReactiveCommand.CreateFromTask(ExportLayout);
+        ExportSourceCommand = ReactiveCommand.CreateFromTask(ExportSource);
+    }
+
+    private async Task ExportLayout()
+    {
+        IStorageFile savePng = await _mainWindow.Window.ShowSaveFilePickerAsync(Strings.Export_Layout_Preview,
+            [new(Strings.PNG_Image) { Patterns = ["*.png"] }], $"{_layout.DisplayName}.png");
+        string path = savePng?.TryGetLocalPath();
+        if (!string.IsNullOrEmpty(path))
+        {
+            await using FileStream fs = File.Create(path);
+            _layout.GetLayoutImage().Encode(fs, SKEncodedImageFormat.Png, GraphicsFile.PNG_QUALITY);
+        }
+    }
+
+    private async Task ExportSource()
+    {
+        if (SelectedLayoutEntry is null)
+        {
+            return;
+        }
+        IStorageFile savePng = await _mainWindow.Window.ShowSaveFilePickerAsync(Strings.Export_Source_Preview,
+            [new(Strings.PNG_Image) { Patterns = ["*.png"] }]);
+        string path = savePng?.TryGetLocalPath();
+        if (!string.IsNullOrEmpty(path))
+        {
+            await using FileStream fs = File.Create(path);
+            SKBitmap preview = SelectedLayoutEntry.FullImage.Copy();
+            SKCanvas canvas = new(preview);
+            canvas.DrawRect(SelectedLayoutEntry.TextureX, SelectedLayoutEntry.TextureY, SelectedLayoutEntry.TextureW, SelectedLayoutEntry.TextureH, new() { Color = SKColors.Red, Style = SKPaintStyle.Stroke });
+            canvas.Flush();
+            preview.Encode(fs, SKEncodedImageFormat.Png, GraphicsFile.PNG_QUALITY);
+        }
     }
 }
 
