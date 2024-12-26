ead8460e6b5f2be3e32c43cd15bbe91b705a1e81
jonko0493 <jonko0493@protonmail.com>
2024-12-26 08:06:31 -0800

    Add MODIFY_FRIENDSHIP command

--- src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
+++ src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
@@ -208,6 +208,7 @@ public class ScriptEditorViewModel : EditorViewModel
                 CommandVerb.CHIBI_EMOTE => new ChibiEmoteScriptCommandEditorViewModel(_selectedCommand, this, _log),
                 CommandVerb.NEXT_SCENE => new EmptyScriptCommandEditorViewModel(_selectedCommand, this, _log),
                 CommandVerb.SKIP_SCENE => new SkipSceneScriptCommandEditorViewModel(_selectedCommand, this, _log),
+                CommandVerb.MODIFY_FRIENDSHIP => new ModifyFriendshipScriptCommandEditorViewModel(_selectedCommand, this, _log),
                 CommandVerb.AVOID_DISP => new EmptyScriptCommandEditorViewModel(_selectedCommand, this, _log),
                 CommandVerb.CHESS_LOAD => new ChessLoadScriptCommandEditorViewModel(_selectedCommand, this, Window, _log),
                 CommandVerb.SCENE_GOTO_CHESS => new SceneGotoScriptCommandEditorViewModel(_selectedCommand, this, _log, Window),
---
+++ src/SerialLoops/Views/Editors/ScriptCommandEditors/ModifyFriendshipScriptCommandEditorView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SerialLoops.Views.Editors.ScriptCommandEditors;
+
+public partial class ModifyFriendshipScriptCommandEditorView : UserControl
+{
+    public ModifyFriendshipScriptCommandEditorView()
+    {
+        InitializeComponent();
+    }
+}
+
