1a4bdb9684376bdbe011c980b10a44b82593955a
jonko0493 <jonko0493@protonmail.com>
2025-01-18 22:21:26 -0800

    Clean unused usings

--- src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
+++ src/SerialLoops/ViewModels/Editors/ScriptEditorViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.Versioning;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
