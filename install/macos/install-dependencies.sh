12f305e35f374ed6a39d06898b5e867dc61a3536
jonko0493 <jonko0493@protonmail.com>
2025-02-28 10:19:21 -0800

    Use absolute path of dkp-pacman

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -3,8 +3,8 @@
 if [ ! -d "/opt/devkitpro/devkitARM" ]; then
     installer -pkg devkitpro-pacman-installer.pkg -target / -dumplog >> /var/tmp/SerialLoopsInstaller.log
     wait
-    dkp-pacman -Sy --noconfirm 2>> /var/tmp/SerialLoopsInstaller.log
-    dkp-pacman -S --noconfirm nds-dev 2>> /var/tmp/SerialLoopsInstaller.log
+    /usr/local/bin/dkp-pacman -Sy --noconfirm 2>> /var/tmp/SerialLoopsInstaller.log
+    /usr/local/bin/dkp-pacman -S --noconfirm nds-dev 2>> /var/tmp/SerialLoopsInstaller.log
     xcode-select --install || true 2>> /var/tmp/SerialLoopsInstaller.log
 fi
 
