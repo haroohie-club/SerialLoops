8c6ef51e7896c670e20905ba860eec1156a820c3
jonko0493 <jonko0493@protonmail.com>
2025-02-27 19:33:44 -0800

    Try the packaging again

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [ ! -d "/opt/devkitpro/devkitARM" ]; then
-    installer -pkg devkitpro-pacman-installer.pkg -target / -dumplog >> /var/tmp/SerialLoopsInstaller.log && \
-    dkp-pacman -Sy --noconfirm >> /var/tmp/SerialLoopsInstaller.log && \
-    dkp-pacman -S --noconfirm nds-dev >> /var/tmp/SerialLoopsInstaller.log && \
+    installer -pkg devkitpro-pacman-installer.pkg -target / -dumplog >> /var/tmp/SerialLoopsInstaller.log
+    dkp-pacman -Sy --noconfirm >> /var/tmp/SerialLoopsInstaller.log
+    dkp-pacman -S --noconfirm nds-dev >> /var/tmp/SerialLoopsInstaller.log
     xcode-select --install || true >> /var/tmp/SerialLoopsInstaller.log
 fi
 
