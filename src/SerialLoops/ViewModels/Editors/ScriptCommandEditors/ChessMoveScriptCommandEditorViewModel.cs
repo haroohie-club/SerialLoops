adc8818d1a8be485610a77886d10ea7ddb17a19f
jonko0493 <jonko0493@protonmail.com>
2025-02-28 11:51:38 -0800

    Start CHESS_MOVE command

--- src/SerialLoops/ViewModels/Editors/ScriptCommandEditors/ChessMoveScriptCommandEditorViewModel.cs
+++ src/SerialLoops/ViewModels/Editors/ScriptCommandEditors/ChessMoveScriptCommandEditorViewModel.cs
@@ -1,6 +1,10 @@
+using HaruhiChokuretsuLib.Util;
+using SerialLoops.Lib.Script;
+
 namespace SerialLoops.ViewModels.Editors.ScriptCommandEditors;
 
-public class ChessMoveScriptCommandEditorViewModel
+public class ChessMoveScriptCommandEditorViewModel(ScriptItemCommand command, ScriptEditorViewModel scriptEditor, ILogger log)
+    : ScriptCommandEditorViewModel(command, scriptEditor, log)
 {
-    
+
 }
