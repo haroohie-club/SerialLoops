c53e24934734712f91d30ca9b021ed7cb7aacc68
jonko0493 <jonko0493@protonmail.com>
2025-02-28 10:13:33 -0800

    Route stderr to log

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -3,9 +3,9 @@
 if [ ! -d "/opt/devkitpro/devkitARM" ]; then
     installer -pkg devkitpro-pacman-installer.pkg -target / -dumplog >> /var/tmp/SerialLoopsInstaller.log
     wait
-    dkp-pacman -Sy --noconfirm >> /var/tmp/SerialLoopsInstaller.log
-    dkp-pacman -S --noconfirm nds-dev >> /var/tmp/SerialLoopsInstaller.log
-    xcode-select --install || true >> /var/tmp/SerialLoopsInstaller.log
+    dkp-pacman -Sy --noconfirm 2>> /var/tmp/SerialLoopsInstaller.log
+    dkp-pacman -S --noconfirm nds-dev 2>> /var/tmp/SerialLoopsInstaller.log
+    xcode-select --install || true 2>> /var/tmp/SerialLoopsInstaller.log
 fi
 
 exit 0
