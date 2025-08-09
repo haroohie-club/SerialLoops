a08d6860c50eeb3d634cea9a20e15f4ce619ee5f
jonko0493 <jonko0493@protonmail.com>
2025-08-09 16:01:55 +0100

    Try invoking zsh

--- install/macos/install-dependencies.sh
+++ install/macos/install-dependencies.sh
@@ -5,12 +5,12 @@ if [ ! -d "/opt/homebrew" ]; then
 fi
 
 if [ ! -d "/opt/homebrew/opt/llvm" ]; then
-    brew install llvm 2>> /var/tmp/SerialLoopsInstaller.log
-    brew install lld 2>> /var/tmp/SerialLoopsInstaller.log
+    /bin/zsh -c "brew install llvm" 2>> /var/tmp/SerialLoopsInstaller.log
+    /bin/zsh -c "brew install lld" 2>> /var/tmp/SerialLoopsInstaller.log
 fi
 
 if [ ! -d "/opt/homebrew/bin/ninja" ]; then
-    brew install ninja 2>> /var/tmp/SerialLoopsInstaller.log
+   /bin/zsh -c "brew install ninja" 2>> /var/tmp/SerialLoopsInstaller.log
 fi
 
 exit 0
